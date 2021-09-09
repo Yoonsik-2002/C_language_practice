27f3ce7e5bebbc46b2ed8603643e527c4280d71e
Yoonsik-2002 <83572199+Yoonsik-2002@users.noreply.github.com>
2021-09-10 00:14:35 +0900

    continue문_Exercise_2
    continue문을 이용하여 만든 1에서 20까지의 정수 중에서 4로 나누어 떨어지지 않는 수를 출력하는 프로그램을 작성하여 보았다.
    for문에서 continue를 만나면 증감 부분으로 이동하여 다음 반복이 계속해서 실행된다.
    이를 이용해서 if (i % 4 == 0), i가 4로 나누어 떨어진 경우, continue를 만나 for문의 증감 부분으로 이동하게 하여 4로 나누어 떨어지지 않는 수를 출력하게 만들었다.

--- continue_exercise_2.c
+++ continue_exercise_2.c
@@ -4,14 +4,14 @@
 
 int main(void)
 {
-	const int MAX = 15;
+	const int MAX = 20; // const상수(const 자료형 상수이름 = 값;) -> 값 고정
 
-	printf("1에서 %d까지 정수 중에서 4로 나누어 떨어지지 않는 수 : \n", MAX);
+	printf("1에서 %d까지 정수 중에서 3으로 나누어 떨어지지 않는 수\n", MAX);
 	for (int i = 1; i <= MAX; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 4 == 0)
 			continue;
-		printf("%3d", i);
+		printf("%3d", i);	
 	}
 	puts("");
 
