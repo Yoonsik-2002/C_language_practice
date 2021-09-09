f4a56839484ba0ad69a00d49e3426bcce9238cb9
Yoonsik-2002 <83572199+Yoonsik-2002@users.noreply.github.com>
2021-09-09 23:17:43 +0900

    break문_Exercise_2
    정수 - 음수, 양수, 0(종료)를 입력받아, 입력받은 수를 출력하고 입력받은 수가 만약 0 일시, 종료하는 프로그램을 작성해 보았다. 
    while (1)은 논리적으로 항상 참(TRUE)이기 때문에, 무한반복이 실행되는데, 이를 break 분기문으로 빠져 나오도록 하였다.

--- break_exercise_1.c
+++ break_exercise_1.c
@@ -2,20 +2,23 @@
 
 #include <stdio.h>
 
-int maid(void)
+int main(void)
 {
-	int input;
+	int input; 
 
 	while (1)
 	{
-		printf("정수[음수, 0(종료), 양수]를 입력후, [Enter] : ");
+		printf("정수[음수, 0(종료), 양수]를 입력 후 [Enter] : ");
 		scanf_s("%d", &input);
 
-		printf("입력한 정수 : %d\n", input);
+		printf("입력 받은 수 : %d\n", input);
+
 		if (input == 0)
+		{
+			printf("\n프로그램을 종료합니다.\n");
 			break;
+		}
 	}
-	puts("\n종료합니다.");
 
 	return 0;
 }
