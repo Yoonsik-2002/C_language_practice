f302e9b17e1b9f924a2cc37e7955311ffd342a1f
Yoonsik-2002 <83572199+Yoonsik-2002@users.noreply.github.com>
2021-09-21 20:14:59 +0900

    중첩반복 : 변수값 출력
    외부반복에서 1 에서 5 까지, 내부반복에서 1 에서 7까지 반복하면서 각각의 변수값을 출력하는 프로그램

---
+++ nested_loop_4.c
@@ -0,0 +1,14 @@
+// 외부반복에서 1 에서 5 까지, 내부반복에서 1 에서 7까지 반복하면서 각각의 변수값을 출력하는 프로그램
+
+/*#include<stdio.h>
+
+int main(void)
+{
+	for (int i = 1; i <= 5; i++) {
+		printf("\ni = % -2d\n", i);
+		for (int j = 1; j <= 7; j++)
+			printf("j = %-3d", j);
+			puts("");
+	}
+	return 0;
+}*/
