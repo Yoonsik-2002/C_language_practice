fbf5d59e12350522cbd5e6ac54345fb68d4f8b9d
Yoonsik-2002 <83572199+Yoonsik-2002@users.noreply.github.com>
2021-09-21 20:15:59 +0900

    '*'삼각형 출력
    중첩된 반복문을 이용하여 '*'을 삼각형 모양으로 출력하는 프로그램

---
+++ nested_loop_5.c
@@ -0,0 +1,19 @@
+// 중첩된 반복문을 이용하여 '*'을 삼각형 모양으로 출력하는 프로그램
+
+/*#include<stdio.h>
+
+int main(void)
+{
+	int n;
+
+	printf("몇층짜리 삼각형을 만들까요? : ");
+	scanf_s("%d", &n);
+	
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i; j++) {
+			printf("*");
+		}
+		puts(""); // puts()는 내부반복 실행후 새로운 줄로 이동하기 위한 문장으로, 외부반복 for문의 반복몸체이다.
+	}
+	return 0;
+}*/
